6186ae045092ee877635ab694ed6eb28f4ee354e
Alexanderpmann <99506481+Alexanderpmann@users.noreply.github.com>
2023-04-20 05:56:55 -0400

    added note about stopping point in tutorial video

--- Java/3d_portfolio/src/components/About.jsx
+++ Java/3d_portfolio/src/components/About.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+// Stopped video at 1:07:37 of 2:53:18 for the Build and Deploy an Amazing 3D Portfolio in React JS | Beginner Three.js Tutorial by JS Mastery Channel
+
 const About = () => {
   return (
     <div>About</div>
