c7a000cf21179813c6d6723658afabef1ebb7923
Alexanderpmann <99506481+Alexanderpmann@users.noreply.github.com>
2023-05-12 17:28:39 -0400

    updating about page

---
+++ MERN/3d_portfolio/src/components/About.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+
+      <motion.p
+        variants={fadeIn("","",0.1,1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          I am a business and marketing professional who has recently transitioned into software development with a passion for creating user-friendly solutions. I have experience in Python, Java, and MERN stack development and specialize in using front-end frameworks like React, Node.js, and Three.js to create visually stunning applications. As a professional photographer on the side, I bring a keen eye for design and aesthetics to my software development work. I am also eager to expand my skills in back-end development to create more robust and efficient software systems. With a collaborative approach and a commitment to problem-solving, I aim to deliver high-quality and scalable solutions that meet my clients' unique needs. Let's build something amazing together!
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+
+      </div>
+
+    </>
+  )
+}
+
+export default About
--- MERN/3d_portfolio/src/constants/index.js
+++ MERN/3d_portfolio/src/constants/index.js
@@ -46,15 +46,15 @@ const services = [
     icon: web,
 },
 {
-    title: "React Native Developer",
+    title: "Front-End Developer",
     icon: mobile,
 },
 {
-    title: "Backend Developer",
+    title: "Back-end Developer",
     icon: backend,
 },
 {
-    title: "Content Creator",
+    title: "Photography",
     icon: creator,
 },
 ];
